e348b5d73489cdcec891a46b2d69931f47288ced
Damien MATTEI <damien.mattei@gmail.com>
2024-08-20 10:56:37 +0200

    scribbling

--- info.rkt
+++ info.rkt
@@ -7,14 +7,12 @@
 (define pkg-desc "SRFI-105 Curly Infix for Racket")
 (define version "9.1")
 (define pkg-authors '(mattei))
-(define scribblings '(("scribblings/scheme-plus.scrbl" ())))
+(define scribblings '(("scribblings/SRFI-105.scrbl" ())))
+(define build-deps '("scribble-lib" "racket-doc" "scribble-code-examples" "scribble-doc"))
 (define license 'LGPL-3.0-or-later)
 
 (define deps
   '("base"
     "srfi-lib"
     "r6rs-lib"))
-
-(define build-deps
-  '())
 
