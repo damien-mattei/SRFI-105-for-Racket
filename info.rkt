ec282cbec1c915a350740d4e6766d73b9a0ab609
Damien MATTEI <damien.mattei@gmail.com>
2024-08-20 11:31:40 +0200

    exclude compiled

--- info.rkt
+++ info.rkt
@@ -2,8 +2,8 @@
 
 (define pkg-name "SRFI-105-for-Racket")
 (define collection "SRFI-105")
-(define compile-omit-paths '("src" "deprecated"))
-(define test-omit-paths '("src"))
+(define compile-omit-paths '("src" "deprecated" "compiled"))
+(define test-omit-paths '("src" "compiled"))
 (define pkg-desc "SRFI-105 Curly Infix for Racket")
 (define version "9.1")
 (define pkg-authors '(mattei))
